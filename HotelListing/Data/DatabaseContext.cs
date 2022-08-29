b3994a83176a1f0540be5d4a9b0b2679f6fa42d8
Nika Nabakhteveli <nabakhtevelin@gmail.com>
2022-08-29 06:33:46 +0400

    Added "AddedDefaultRoles" migration

--- HotelListing/Data/DatabaseContext.cs
+++ HotelListing/Data/DatabaseContext.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+using HotelListing.Configurations.Entities;
+
 namespace HotelListing.Data;
 
 // Bridge between application and database
@@ -17,60 +19,8 @@ public class DatabaseContext : IdentityDbContext<ApiUser>
     {
         base.OnModelCreating(builder);
         
-        builder.Entity<Country>().HasData(
-            new Country
-            {
-                Id = 1,
-                Name = "Georgia",
-                ShortName = "GEO"
-            },
-            new Country
-            {
-                Id = 2,
-                Name = "Germany",
-                ShortName = "DEU"
-            },
-            new Country
-            {
-                Id = 3,
-                Name = "Japan",
-                ShortName = "JPN"
-            }
-        );
-
-        builder.Entity<Hotel>().HasData(
-            new Hotel
-            {
-                Id = 1,
-                Name = "Hotel 1",
-                Address = "Hotel 1 Address",
-                Rating = 4.5,
-                CountryId = 1,
-            },
-            new Hotel
-            {
-                Id = 2,
-                Name = "Hotel 2",
-                Address = "Hotel 2 Address",
-                Rating = 4.9,
-                CountryId = 2,
-            },
-            new Hotel
-            {
-                Id = 3,
-                Name = "Hotel 3",
-                Address = "Hotel 3 Address",
-                Rating = 4.4,
-                CountryId = 1,
-            },
-            new Hotel
-            {
-                Id = 4,
-                Name = "Hotel 4",
-                Address = "Hotel 4 Address",
-                Rating = 4.7,
-                CountryId = 3,
-            }
-        );
+        builder.ApplyConfiguration(new RoleConfiguration());
+        builder.ApplyConfiguration(new CountryConfiguration());
+        builder.ApplyConfiguration(new HotelConfiguration());
     }
 }
---
+++ HotelListing/Migrations/20220828134004_AddedDefaultRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "9277bd7c-47e0-4f8e-88fb-05e6081b680b", "5a5ce5eb-1e96-450c-ac6e-569265e576cc", "User", "USER" },
+                    { "b6832497-3f9c-4146-aef0-62c514ae6133", "497e414b-9f7d-485d-bc06-589c7e6e96bb", "admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9277bd7c-47e0-4f8e-88fb-05e6081b680b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b6832497-3f9c-4146-aef0-62c514ae6133");
+        }
+    }
+}
